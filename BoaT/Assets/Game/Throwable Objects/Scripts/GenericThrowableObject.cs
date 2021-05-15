15eda26a21fc5ef2f53d495be8536ea273012f44
Alex Silvestri <alexsilvestri.16@gmail.com>
2021-05-15 15:15:20 +0200

    Fix to feedback color display when throwable object is in hand

--- BoaT/Assets/Game/Throwable Objects/Scripts/GenericThrowableObject.cs
+++ BoaT/Assets/Game/Throwable Objects/Scripts/GenericThrowableObject.cs
@@ -3,6 +3,7 @@
 public class GenericThrowableObject : MonoBehaviour, IThrowable
 {
     public float Weight { get; set; }
+    private bool isAttachedToHand;
     private Collider physicsCollider;
     private GameObject baseContainer;
     private Color baseColor;
@@ -16,11 +17,13 @@ public class GenericThrowableObject : MonoBehaviour, IThrowable
 
     void Start()
     {
+        isAttachedToHand = false;
         this.gameObject.transform.SetParent(baseContainer.transform);
     }
 
     public void AttachToPlayer(GameObject playerHand)
     {
+        isAttachedToHand = true;
         this.gameObject.GetComponent<Rigidbody>().useGravity = false;
         this.gameObject.transform.position = playerHand.transform.position;
         this.gameObject.transform.SetParent(playerHand.transform);
@@ -28,6 +31,7 @@ public class GenericThrowableObject : MonoBehaviour, IThrowable
     }
     public void DetachFromPlayer()
     {
+        isAttachedToHand = false;
         this.gameObject.transform.SetParent(baseContainer.transform);
         physicsCollider.enabled = true;
         this.gameObject.GetComponent<Rigidbody>().useGravity = true;
@@ -36,7 +40,14 @@ public class GenericThrowableObject : MonoBehaviour, IThrowable
     public void Highlighted(bool isHighlighted)
     {
         Material mat = this.gameObject.GetComponent<Renderer>().material;
-        if (isHighlighted) mat.color = Color.red;
-        else mat.color = baseColor;
+        if (isAttachedToHand == false)
+        {
+            if (isHighlighted) mat.color = Color.red;
+            else mat.color = baseColor;
+        }
+        else
+        {
+            mat.color = baseColor;
+        }
     }
 }
