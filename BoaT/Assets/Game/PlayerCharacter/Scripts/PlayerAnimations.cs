bd0273ed16a05ba0c3c7968984944dd2ebe85e64
Alex Silvestri <alexsilvestri.16@gmail.com>
2021-05-23 16:17:20 +0200

    animator first setup completed

---
+++ BoaT/Assets/Game/PlayerCharacter/Scripts/PlayerAnimations.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    [SerializeField] private Animator playerAnimator;
+    private PlayerCharacter playerCharacter;
+    private string actualState;
+
+    private void Awake()
+    {
+        playerCharacter = this.gameObject.GetComponent<PlayerCharacter>();
+    }
+    private void Start()
+    {
+        SetupStateBool();
+    }
+    private void Update()
+    {
+        AnimatorStateUpdate();
+    }
+
+    private void AnimatorStateUpdate()
+    {
+        if (actualState != playerCharacter.stateRef && playerCharacter.stateRef != "Hands")
+        {
+            playerAnimator.SetBool(actualState, false);
+            SetupStateBool();
+        }
+    }
+    private void SetupStateBool()
+    {
+        actualState = playerCharacter.stateRef;
+        playerAnimator.SetBool(actualState, true);
+    }
+}
--- BoaT/Assets/Game/Scripts/StateMachine.cs
+++ BoaT/Assets/Game/Scripts/StateMachine.cs
@@ -2,10 +2,12 @@
 public abstract class StateMachine : MonoBehaviour
 {
     protected PlayerState _state;
+    [HideInInspector] public string stateRef;
 
     public void SetState(PlayerState state)
     {
         _state = state;
+        stateRef = _state.ToString();
         state.Start();
     }
 }
