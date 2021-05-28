da5d4f07654dcc5d1fbec652e946203c84280bfa
Alex Silvestri <alexsilvestri.16@gmail.com>
2021-05-28 17:14:33 +0200

    Added playerDetectionRange to soul

---
+++ BoaT/Assets/Game/Souls/Scripts/PlayerInRange.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerInRange : MonoBehaviour
+{
+    private SoulController soulController;
+
+    private void Awake()
+    {
+        soulController = this.gameObject.transform.parent.GetComponent<SoulController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) soulController.playerIsInRange = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) soulController.playerIsInRange = false;
+    }
+}
--- BoaT/Assets/Game/Souls/Scripts/SoulController.cs
+++ BoaT/Assets/Game/Souls/Scripts/SoulController.cs
@@ -9,29 +9,12 @@ public class SoulController : MonoBehaviour
     {
         soulReferences = this.gameObject.GetComponent<SoulReferences>();
     }
-    private void GetPlayerInRange()
-    {
-        bool playerFound = false;
-        Collider[] colliders = Physics.OverlapSphere(this.transform.position, soulDetectionRange);
-        foreach (Collider collider in colliders)
-        {
-            if (collider.CompareTag("Player"))
-            {
-                playerFound = true;
-                playerIsInRange = true;
-                break;
-            }
-        }
-        if (playerFound) playerIsInRange = false;
-    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) playerIsInRange = true;
         if (other.CompareTag("StorageRoom")) isInsideStorageRoom = true;
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player")) playerIsInRange = false;
         if (other.CompareTag("StorageRoom")) isInsideStorageRoom = false;
     }
 }
