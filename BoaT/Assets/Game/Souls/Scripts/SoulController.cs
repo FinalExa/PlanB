19cf0642b4dc0fc0b68291b11cb1fca76bf3e0df
Alex Silvestri <alexsilvestri.16@gmail.com>
2021-06-03 16:27:43 +0200

    Soul escaping player is now responsive

--- BoaT/Assets/Game/Souls/Scripts/SoulController.cs
+++ BoaT/Assets/Game/Souls/Scripts/SoulController.cs
@@ -3,12 +3,13 @@ using UnityEngine.AI;
 public class SoulController : MonoBehaviour
 {
     public bool isInsideStorageRoom;
+    public bool collidedWithOther;
     public NavMeshAgent thisNavMeshAgent;
     public Rigidbody thisRigidbody;
     [HideInInspector] public GameObject playerIsInRange;
     [HideInInspector] public SoulReferences soulReferences;
     [HideInInspector] public GameObject exit;
-    [SerializeField] private float soulDetectionRange;
+    //[SerializeField] private float soulDetectionRange;
     private void Awake()
     {
         soulReferences = this.gameObject.GetComponent<SoulReferences>();
@@ -25,6 +26,14 @@ public class SoulController : MonoBehaviour
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("StorageRoom")) isInsideStorageRoom = false;
+        if (other.CompareTag("StorageRoom") && !other.CompareTag("Player")) isInsideStorageRoom = false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Ground") && !collision.gameObject.CompareTag("Player")) collidedWithOther = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Ground") && !collision.gameObject.CompareTag("Player")) collidedWithOther = false;
     }
 }
--- BoaT/Assets/Game/Souls/Scripts/SoulStateMachine/SoulEscapePlayer.cs
+++ BoaT/Assets/Game/Souls/Scripts/SoulStateMachine/SoulEscapePlayer.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 public class SoulEscapePlayer : SoulState
 {
     public SoulEscapePlayer(SoulStateMachine soulStateMachine) : base(soulStateMachine)
@@ -6,25 +7,29 @@ public class SoulEscapePlayer : SoulState
     }
     public override void Start()
     {
-        SetupEscape();
+        SetupEscape(false);
     }
     public override void StateUpdate()
     {
-        EndEscape();
+        EscapeCheck();
     }
-    private void SetupEscape()
+    private void SetupEscape(bool reversePath)
     {
         if (_soulStateMachine.soulController.playerIsInRange != null && _soulStateMachine.soulController.thisNavMeshAgent.enabled == true)
         {
             Vector3 playerPos = _soulStateMachine.soulController.playerIsInRange.transform.position;
             Vector3 soulPos = _soulStateMachine.soulController.gameObject.transform.position;
-            Vector3 destination = new Vector3(soulPos.x + (soulPos.x - playerPos.x), soulPos.y, soulPos.z + (soulPos.z - playerPos.z));
+            Vector3 destination;
+            if (!reversePath) destination = new Vector3(soulPos.x + (soulPos.x - playerPos.x), soulPos.y, soulPos.z + (soulPos.z - playerPos.z));
+            else destination = new Vector3(soulPos.x - (soulPos.x - playerPos.x), soulPos.y, soulPos.z - (soulPos.z - playerPos.z));
             _soulStateMachine.soulController.thisNavMeshAgent.isStopped = false;
             _soulStateMachine.soulController.thisNavMeshAgent.SetDestination(destination);
         }
     }
-    private void EndEscape()
+    private void EscapeCheck()
     {
+        if (_soulStateMachine.soulController.thisNavMeshAgent.pathStatus == NavMeshPathStatus.PathComplete) SetupEscape(false);
+        if (_soulStateMachine.soulController.collidedWithOther == true) SetupEscape(true);
         Transitions();
     }
     #region Transitions
