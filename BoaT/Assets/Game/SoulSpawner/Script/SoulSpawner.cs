5f6ef980a9410d41a10a546459b8725bab3c33dd
Alex Silvestri <alexsilvestri.16@gmail.com>
2021-06-04 16:23:07 +0200

    Spawner works when enterint/exiting storage room

---
+++ BoaT/Assets/Game/SoulSpawner/Script/SoulSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+public class SoulSpawner : Spawner
+{
+    private BoxCollider thisTrigger;
+
+    public override void Awake()
+    {
+        base.Awake();
+        thisTrigger = this.gameObject.GetComponent<BoxCollider>();
+    }
+
+    public override void Start()
+    {
+        CalculateObjectsToInstantiate();
+        base.Start();
+    }
+    void CalculateObjectsToInstantiate()
+    {
+        objectsToInstantiate = (int)(((thisTrigger.size.x - 1) * (thisTrigger.size.z - 1)) / 3);
+        if (objectsToSpawn < 0 || objectsToSpawn > objectsToInstantiate) objectsToSpawn = (int)Mathf.Lerp(0, objectsToInstantiate, 1);
+    }
+
+    private void AssignCoordinatesToObjects()
+    {
+        ActivateObjects(new Vector3(0f, 0f, 0f));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) AssignCoordinatesToObjects();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) DeactivateObjects();
+    }
+}
---
+++ BoaT/Assets/Game/Throwable Objects/Scripts/Spawner.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Spawner : MonoBehaviour
+{
+    protected int objectsToInstantiate;
+    [SerializeField] protected int objectsToSpawn;
+    [SerializeField] private GameObject objectReference;
+    [SerializeField] private List<GameObject> objects;
+    [SerializeField] private List<GameObject> activeObjects;
+    [SerializeField] private string parentObjectTag;
+    private GameObject parentObject;
+
+    public virtual void Awake()
+    {
+        parentObject = GameObject.FindGameObjectWithTag(parentObjectTag);
+    }
+
+    public virtual void Start()
+    {
+        CreateObjects();
+    }
+
+    public virtual void CreateObjects()
+    {
+        for (int i = 0; i < objectsToInstantiate; i++)
+        {
+            objects.Add(Instantiate(objectReference, parentObject.transform));
+            objects[i].SetActive(false);
+        }
+    }
+    public virtual void ActivateObjects(Vector3 positionToActivate)
+    {
+        if (activeObjects.Count == 0)
+        {
+            int countSpawnedObjects = 0;
+            for (int i = 0; i < objectsToInstantiate; i++)
+            {
+                if (!objects[i].activeSelf)
+                {
+                    objects[i].transform.localPosition = positionToActivate;
+                    objects[i].SetActive(true);
+                    activeObjects.Add(objects[i]);
+                    countSpawnedObjects++;
+                }
+                if (countSpawnedObjects == objectsToSpawn) break;
+            }
+        }
+    }
+    public virtual void DeactivateObjects()
+    {
+        for (int i = 0; i < activeObjects.Count; i++)
+        {
+            activeObjects[i].SetActive(false);
+        }
+        activeObjects.Clear();
+    }
+}
